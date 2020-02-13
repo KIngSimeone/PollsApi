b7b18556bc63b8f95cd05fcaa8a7bff29eaae23e
KingSimeone <simeoneumoh@outlook.com>
2020-02-13 13:14:25 +0100

    Login api

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -3,7 +3,7 @@ from .views import polls_list, polls_detail
 #alternative with apiviews
 from .apiviews import PollList, PollDetail, ChoiceList, CreateVote, UserCreate
 from rest_framework.routers import DefaultRouter
-from .apiviews import PollViewSet
+from .apiviews import PollViewSet, LoginView
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename= 'polls')
@@ -19,6 +19,7 @@ urlpatterns = [
     path('polls/<int:pk>/choices/', ChoiceList.as_view(), name="choice_list"),
     path('polls/<int:pk>/choices/<int:choice_pk/vote/', CreateVote.as_view(), name = "create_vote"),
     path('users/', UserCreate.as_view(), name="user_create"),
+    path('login/',LoginView.as_view(), name="login")
 ]
 
 urlpatterns += router.urls
