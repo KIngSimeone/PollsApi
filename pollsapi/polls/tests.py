8e6fe2f22b7209229501767d236fccd393d8c61b
KingSimeone <simeoneumoh@outlook.com>
2020-02-19 10:44:04 +0100

    Add user authentication for test

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
-from django
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 from polls import apiviews
 # Create your tests here.
@@ -11,9 +12,23 @@ class TestPoll(APITestCase):
         self.factory = APIRequestFactory()
         self.view = apiviews.PollViewSet.as_view({'get':'list'})
         self.uri = '/polls'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@test.com',
+            password='test'
+        )
 
     def test_list(self):
-        request = self.factory.get(self.uri)
+        request = self.factory.get(self.uri,
+            HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+        request.user = self.user
         response = self.view(request)
         self.assertEqual(response.status_code,200,
                         'Expected Response Code 200, recieved {0} instead.'
